5818e62ce514dcbc446439c35282805b30eefdf8
gk400282 <gk400282@gmail.com>
2019-01-06 00:21:20 +0530

    Updates

---
+++ precache-manifest.7ae1a423d76ac317526027dfa6934ad5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9cb24a017826204c3bab",
+    "url": "/seasons/static/css/main.8845594e.chunk.css"
+  },
+  {
+    "revision": "9cb24a017826204c3bab",
+    "url": "/seasons/static/js/main.9cb24a01.chunk.js"
+  },
+  {
+    "revision": "619bf66c065dd1aac483",
+    "url": "/seasons/static/js/1.619bf66c.chunk.js"
+  },
+  {
+    "revision": "b162515c83c554475e0d",
+    "url": "/seasons/static/js/runtime~main.b162515c.js"
+  },
+  {
+    "revision": "983df0a98b4f2183d401d79024918994",
+    "url": "/seasons/index.html"
+  }
+];
